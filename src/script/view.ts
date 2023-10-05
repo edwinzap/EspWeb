92ec8db0da954046c61737ab874ccd2ff0bab1c1
Miguel <forgetmiguel@hotmail.com>
2023-10-05 10:10:46 +0200

    Fix watch script

--- src/script/view.ts
+++ src/script/view.ts
@@ -1,4 +1,4 @@
-import { Project } from "./project";
+import { Project } from "./project.js";
 
 /**
  * "view.ts" contient les fonctions/éléments permettant d'initialiser et d'accéder aux éléments visuels de la page.
